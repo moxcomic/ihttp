48f243e033a559423aa4e6a385988892090c1212
moxcomic <37604141+moxcomic@users.noreply.github.com>
2023-07-29 23:04:17 +0800

    format

--- request_test.go
+++ request_test.go
@@ -13,7 +13,10 @@ func TestError(t *testing.T) {
 		s string
 	)
 
-	s = New().Get().WithError(func(err error) { e = err }).ToString()
+	s = New().
+		Get().
+		WithError(func(err error) { e = err }).
+		ToString()
 
 	if e != nil {
 		panic(e)
@@ -23,18 +26,34 @@ func TestError(t *testing.T) {
 }
 
 func TestGetString(t *testing.T) {
-	fmt.Println(New().WithUrl("https://httpbin.org/get").Get().WithError(func(err error) { panic(err) }).ToString())
+	fmt.Println(
+		New().
+			WithUrl("https://httpbin.org/get").
+			Get().
+			WithError(func(err error) { panic(err) }).
+			ToString(),
+	)
 }
 
 func TestGetJson(t *testing.T) {
-	v := New().WithUrl("https://httpbin.org/get").Get().WithError(func(err error) { panic(err) }).ToJson()
+	v := New().
+		WithUrl("https://httpbin.org/get").
+		Get().
+		WithError(func(err error) { panic(err) }).
+		ToJson()
+
 	fmt.Println(v)
 	fmt.Println(v == nil)
 	fmt.Println(v.GetString("origin"))
 }
 
 func TestGetGson(t *testing.T) {
-	v := New().WithUrl("https://httpbin.org/get").Get().WithError(func(err error) { panic(err) }).ToGson()
+	v := New().
+		WithUrl("https://httpbin.org/get").
+		Get().
+		WithError(func(err error) { panic(err) }).
+		ToGson()
+
 	fmt.Println(v)
 	fmt.Println(v.Nil())
 	fmt.Println(v.Get("origin").Str())
@@ -45,27 +64,78 @@ func TestGetStruct(t *testing.T) {
 		Origin string `json:"origin"`
 	}{}
 
-	New().WithUrl("https://httpbin.org/get").Get().WithError(func(err error) { panic(err) }).ToJsonStruct(&resp)
+	New().
+		WithUrl("https://httpbin.org/get").
+		Get().
+		WithError(func(err error) { panic(err) }).
+		ToJsonStruct(&resp)
 
 	fmt.Println(resp.Origin)
 }
 
 func TestGet301(t *testing.T) {
-	fmt.Println(New().WithUrl("https://b23.tv/26kpzlf").WithHijackRedirect().WithError(func(err error) { panic(err) }).Head().ToLocation())
+	fmt.Println(
+		New().
+			WithUrl("https://b23.tv/26kpzlf").
+			WithHijackRedirect().
+			WithError(func(err error) { panic(err) }).
+			Head().
+			ToLocation(),
+	)
 }
 
 func TestProxy(t *testing.T) {
-	fmt.Println(New().WithUrl("https://httpbin.org/get").WithLocalHttpProxy(7890).Get().WithError(func(err error) { panic(err) }).ToString())
+	fmt.Println(
+		New().
+			WithUrl("https://httpbin.org/get").
+			WithLocalHttpProxy(7890).
+			Get().
+			WithError(func(err error) { panic(err) }).
+			ToString(),
+	)
 }
 
 func TestTimeout(t *testing.T) {
-	fmt.Println(New().WithUrl("https://httpbin.org/get").WithTimeout(time.Millisecond).Get().WithError(func(err error) { panic(err) }).ToString())
+	fmt.Println(
+		New().
+			WithUrl("https://httpbin.org/get").
+			WithTimeout(time.Millisecond).
+			Get().
+			WithError(func(err error) { panic(err) }).
+			ToString(),
+	)
 }
 
 func TestQuery(t *testing.T) {
-	fmt.Println(New().WithUrl("https://httpbin.org/get").WithQuery(url.Values{"a": {"b"}}).Get().WithError(func(err error) { panic(err) }).ToJson().GetString("url"))
+	fmt.Println(
+		New().
+			WithUrl("https://httpbin.org/get").
+			WithQuery(url.Values{"a": {"b"}}).
+			Get().
+			WithError(func(err error) { panic(err) }).
+			ToJson().
+			GetString("url"),
+	)
 	<-time.After(time.Second * 2)
-	fmt.Println(New().WithUrl("https://httpbin.org/get").WithAddQuery("a", "b").Get().WithError(func(err error) { panic(err) }).ToJson().GetString("url"))
+
+	fmt.Println(
+		New().
+			WithUrl("https://httpbin.org/get").
+			WithAddQuery("a", "b").
+			Get().
+			WithError(func(err error) { panic(err) }).
+			ToJson().
+			GetString("url"),
+	)
+
 	<-time.After(time.Second * 2)
-	fmt.Println(New().WithUrl("https://httpbin.org/get").WithAddQuerys(map[string]string{"a": "b", "c": "d"}).Get().WithError(func(err error) { panic(err) }).ToJson().GetString("url"))
+	fmt.Println(
+		New().
+			WithUrl("https://httpbin.org/get").
+			WithAddQuerys(map[string]string{"a": "b", "c": "d"}).
+			Get().
+			WithError(func(err error) { panic(err) }).
+			ToJson().
+			GetString("url"),
+	)
 }
